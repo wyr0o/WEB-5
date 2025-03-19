9e35c792e0e2bef315f4cab7f35fc8db21b052af
wyr0o <brykin-2006@mail.ru>
2025-03-19 11:36:40 +0300

    Add files via upload

---
+++ вебинар 5 задание 2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	int size = sizeof(arr) / sizeof(arr[0]);
+
+	int minval = arr[0];
+	int maxval = arr[0];
+
+	cout << "массив: ";
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] < minval)
+		{
+			minval = arr[i];
+		}
+		if (arr[i] > minval)
+		{
+			maxval = arr[i];
+		}
+	}
+
+	cout <<endl<<  "минимальный элемент: " << minval; 
+	cout <<endl<<  "максимальны элемент: " << maxval;
+}
